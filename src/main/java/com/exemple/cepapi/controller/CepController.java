0eb69b2db83efc97fa242cc629035fb1d151bc51
Lara Menck <lara.menckt@gmail.com>
2023-06-29 10:49:03 -0300

    findByAddress request param annotation

--- src/main/java/com/exemple/cepapi/controller/CepController.java
+++ src/main/java/com/exemple/cepapi/controller/CepController.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.exemple.cepapi.client.response.CepResponse;
@@ -20,15 +21,15 @@ public class CepController {
     
     private final CepService service;
 
-    @GetMapping("/{cep}")
+    @GetMapping("cep/{cep}")
     public ResponseEntity<CepResponse> findByCep(@PathVariable String cep){
         return ResponseEntity.ok().body(service.findByCep(cep));
     }
 
-    @GetMapping("{uf}/{localidade}/{logradouro}")
-    public ResponseEntity<List<CepResponse>> findByAddress(@PathVariable(value = "uf") String uf, 
-                                                        @PathVariable(value = "localidade") String localidade, 
-                                                        @PathVariable(value = "logradouro") String logradouro){
+    @GetMapping("endereco/")
+    public ResponseEntity<List<CepResponse>> findByAddress(@RequestParam(value = "uf") String uf, 
+                                                        @RequestParam(value = "localidade") String localidade, 
+                                                        @RequestParam(value = "logradouro") String logradouro){
         return ResponseEntity.ok().body(service.findByAddress(uf, localidade, logradouro));
     }
 }
