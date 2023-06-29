3811a9e981b504f2c8393b266bdd454fa6224a0f
Lara Menck <lara.menckt@gmail.com>
2023-06-29 11:07:05 -0300

    PathVariable annotation

--- src/main/java/com/exemple/cepapi/controller/CepController.java
+++ src/main/java/com/exemple/cepapi/controller/CepController.java
@@ -6,7 +6,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.exemple.cepapi.client.response.CepResponse;
@@ -26,10 +25,10 @@ public class CepController {
         return ResponseEntity.ok().body(service.findByCep(cep));
     }
 
-    @GetMapping("endereco/")
-    public ResponseEntity<List<CepResponse>> findByAddress(@RequestParam(value = "uf") String uf, 
-                                                        @RequestParam(value = "localidade") String localidade, 
-                                                        @RequestParam(value = "logradouro") String logradouro){
+    @GetMapping("edereco/{uf}/{localidade}/{logradouro}")
+    public ResponseEntity<List<CepResponse>> findByAddress(@PathVariable(value = "uf") String uf, 
+                                                        @PathVariable(value = "localidade") String localidade, 
+                                                        @PathVariable(value = "logradouro") String logradouro){
         return ResponseEntity.ok().body(service.findByAddress(uf, localidade, logradouro));
     }
 }
